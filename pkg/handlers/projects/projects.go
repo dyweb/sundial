3f6d741063ac6c2f1a56613d4a400685a65ecc37
Ruiyang Wang <xplorld@gmail.com>
2018-12-29 00:53:54 -0800

    add project API (#13)

--- pkg/handlers/projects/projects.go
+++ pkg/handlers/projects/projects.go
@@ -4,18 +4,16 @@ import (
 	"context"
 
 	"github.com/dyweb/sundial/pkg/models"
+	"github.com/dyweb/sundial/pkg/store/rdb"
 )
 
 // GetProjects returns the list of projects for the given user.
 func GetProjects(ctx context.Context, username string) ([]models.Project, error) {
-	// ds := rdb.FromContext(ctx)
-	// err := ds.CreateProject(&models.Project{
-	// 	Name: "test",
-	// })
-	return []models.Project{}, nil
+	ds := rdb.FromContext(ctx)
+	return ds.GetProjects()
 }
 
 // GetCurrentProjects returns the list of projects for the current user.
 func GetCurrentProjects(ctx context.Context) ([]models.Project, error) {
-	return []models.Project{}, nil
+	return GetProjects(ctx, "")
 }
